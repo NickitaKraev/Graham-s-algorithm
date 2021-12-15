53f08c4413caa5d27ca6790d7a9aeae3dc9804e0
NickitaKraev <75929318+NickitaKraev@users.noreply.github.com>
2021-12-15 22:07:01 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
     ofstream fout;
-    fout.open("Graham_Algorithm.txt");
+    fout.open("result.txt");
     int number_points[14] = { 5 , 10, 15, 20, 30, 50, 75, 100, 500, 1000, 2500, 5000, 7500, 10000 };
     for (int i = 0; i < 14; i++) {
         fout << number_points[i] << " ";
@@ -14,10 +14,7 @@ int main()
     bool flag[2] = { true, false };
     double time_QuikSort[14];
     double time_AVL_Tree[14];
-    GrahamScan obj;
-    vector<Point> points = obj.Get_Random_points(number_points[0]);
-    vector<Point> res = obj.Calculate(points, flag[1]);
-    int size = res.size();
+
     for (int i = 0; i < 14; i++) {
         GrahamScan obj;        
         vector<Point> points = obj.Get_Random_points(number_points[i]);
@@ -27,12 +24,18 @@ int main()
         int size = res.size();
         auto duration = duration_cast<microseconds>(stop - start);
         time_QuikSort[i] = double(duration.count())/ 1000000.0;
+        cout << "Быстрая сортировка" << endl;
+        cout << "Количество точек: " << number_points[i] << endl << "Время выполнения: " << time_QuikSort[i] << endl;
+        cout << "Количество точек в конечном множестве: " << size << endl;
+        for (int i = 0; i < size; i++) { cout << res[i].x << " " << res[i].y << endl; }
+        cout << endl;
     }
+
     fout << "\n";
     for (int i = 0; i < 14; i++) {
-        cout << number_points[i] << " " << time_QuikSort[i] << endl;
         fout << time_QuikSort[i] << " ";
     }
+
     for (int i = 0; i < 14; i++) {
         GrahamScan obj;
         vector<Point> points = obj.Get_Random_points(number_points[i]);
@@ -42,10 +45,15 @@ int main()
         int size = res.size();
         auto duration = duration_cast<microseconds>(stop - start);
         time_AVL_Tree[i] = double(duration.count()) / 1000000.0;
+        cout << "АВЛ-дерево" << endl;
+        cout << "Количество точек: " << number_points[i] << endl << "Время выполнения: " << time_AVL_Tree[i] << endl;
+        cout << "Количество точек в конечном множестве: " << size << endl;
+        for (int i = 0; i < size; i++) { cout << res[i].x << " " << res[i].y << endl; }
+        cout << endl;
     }
+
     fout << "\n";
     for (int i = 0; i < 14; i++) {
-        cout << number_points[i]<< " " << time_AVL_Tree[i] << endl;
         fout << time_AVL_Tree[i] << " ";
     }
     fout.close();
